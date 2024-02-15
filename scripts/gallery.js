adb605c1d29027b3e9a70a803383275fa9a28e49
rei suo <re@is.uo>
2024-02-15 14:46:43 +0900

    Initial commit

---
+++ scripts/gallery.js
@@ -0,0 +1,111 @@
+//
+//
+//     Note:  Images are named "X.jpg" where X is a number from 0 to 999,
+//            and that they are all located in the "webgallery/Pictures" folder.
+//
+//
+//
+// ======  Setting constants ========
+//
+const amountOfPicures = 205; //_________________________Amount of pictures in folder "Pictures"
+const refresh = 60; //__________________________________Page Refresh rate in seconds
+const baseUrl = 'pictures'; //__________________________Base URL
+//
+//
+// ======  Set up photo grid ========
+//
+const photoGrid = document.getElementById("photo-grid");
+let imageIndexes = [];
+
+const refreshRate = refresh*1000; 
+
+console.log("Images in rotation: " + amountOfPicures);
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function changeRandomImage() {
+  const cells = photoGrid.querySelectorAll(".cell");
+  const randomIndex = getRandomInt(0, cells.length - 1);
+  const cell = cells[randomIndex];
+  const image = cell.querySelector("img");
+  const randomImageIndex = checkDouble(); //getRandomInt(1, amountOfPicures);
+  const imageUrl = `${baseUrl}/${randomImageIndex}.jpg`;
+
+  image.classList.remove('fade-in');
+  void image.offsetWidth;
+
+  image.setAttribute("src", imageUrl);
+
+  image.classList.add('fade-in');
+}
+
+function checkDouble() {
+  let randomImageIndex = getRandomInt(1, amountOfPicures);
+
+  while (imageIndexes.includes(randomImageIndex)) {
+    randomImageIndex = getRandomInt(1, amountOfPicures);
+  }
+
+  // imageIndexes.includes(randomImageIndex)
+  //   ? console.log("Image is not Unigue: " + randomImageIndex)
+  //   : console.log("Image is Unigue: " + randomImageIndex);
+
+  imageIndexes.push(randomImageIndex);
+  if (imageIndexes.length > 100) {
+    imageIndexes.shift();
+  }
+
+  // console.log("Array length: " + imageIndexes.length);
+  // console.log("Array: " + imageIndexes);
+
+  return imageIndexes[imageIndexes.length - 1];
+}
+
+setInterval(changeRandomImage, refreshRate);
+
+// Load initial images
+for (let i = 1; i < 10; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+
+  const link = document.createElement("a");
+  const image = document.createElement("img");
+  const randomImageIndex = checkDouble();
+  const imageUrl = `${baseUrl}/${randomImageIndex}.jpg`;
+  image.setAttribute("src", imageUrl);
+  link.setAttribute("href", imageUrl);
+  // Random picture link
+  // link.setAttribute("href", "https://picsum.photos/2560/1600");
+
+  link.appendChild(image);
+  cell.appendChild(link);
+  photoGrid.appendChild(cell);
+}
+//
+// ======  Set up clock ========
+//
+function updateClock() {
+  const clockElement = document.getElementById("clock");
+  const day = document.getElementById("dayOfWeek");
+  const dayOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const now = new Date();
+  const timeString = now.toLocaleTimeString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    // weekday: "short",
+  });
+  let days = now.getDay();
+  clockElement.innerText = timeString;
+  day.innerHTML = dayOfWeek[days];
+}
+setInterval(updateClock, 1000);
---
+++ scripts/weather.js
@@ -0,0 +1,107 @@
+//
+// TODO: 
+//      - Refresh every 30 minutes - DONE
+//      - Add a default city - DONE
+//      - Dim a search button when the input is empty ?
+//      - Input field has only underlined border - DONE
+//      - Add a loading spinner ?
+//      - Add a cookie to remember the last city - DONE
+//      - Hide / Show weather widget.
+//
+//
+// ======  Setting constants ========
+//
+let defaultCity = 'Stockholm'; //_________________________________________ Default city
+let refreshWeather = 15; //_______________________________________________ Page Refresh rate in minutes
+//
+//
+// ======  Fetching weather data ========
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+const apiKey =  "935dab6fc9876743c9ec8fba2e687c63";
+
+// Create a single list item for the city
+const li = document.createElement("li");
+li.classList.add("city");
+list.appendChild(li);
+
+// Function to fetch weather data
+function fetchWeather(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://openweathermap.org/img/wn/${
+        weather[0]["icon"]
+      }@2x.png`;
+
+      const markup = `
+        <div class="city-info">
+          <h2 class="city-name" data-name="${name},${sys.country}">
+            <span>${name}</span>
+            <sup>${sys.country}</sup>
+          </h2>
+          <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+          <figcaption>${weather[0]["description"]}</figcaption>
+          <figure>
+            <img class="city-icon" src="${icon}" alt="${weather[0]["description"]}">
+          </figure>
+        </div>
+      `;
+      li.innerHTML = markup;
+    })
+    .catch(() => {
+      msg.textContent = "Please enter a city name";
+    });
+}
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    let date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; SameSite=Lax; path=/";
+}
+
+// Function to get a cookie
+function getCookie(name) {
+  let nameEQ = name + "=";
+  let ca = document.cookie.split(';');
+  for(let i=0;i < ca.length;i++) {
+    let c = ca[i];
+    while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+// Fetch weather data for default city
+let currentCity = getCookie('city') || defaultCity;
+fetchWeather(currentCity);
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  currentCity = input.value;
+
+  // Save city in cookie for 1800 days
+  setCookie('city', currentCity, 1800);
+
+  fetchWeather(currentCity);
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
+
+// Update weather every X minutes
+setInterval(() => {
+  fetchWeather(currentCity);
+}, refreshWeather * 60 * 1000); 
