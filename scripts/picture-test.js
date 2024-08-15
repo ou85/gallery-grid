25a5bda149df866d3afbba374985a47c79ebde96
rei suo <re@is.uo>
2024-08-15 15:01:24 +0900

    feat: Update image paths and fix broken links

--- scripts/picture-test.js
+++ scripts/picture-test.js
@@ -1,12 +1,9 @@
 const preload = 15;
-const gridSize = 12;
 const picBuffer = 30;
 const indexBuffer = 3;
 const refreshRate = 60 * 1000; 
 const preloadThreshold = preload - 5;
 const photoGrid = document.getElementById("photo-grid");
-// const cloudUrl = "https://res.cloudinary.com/dacsww4tg/image/upload/c_scale,w_300/q_auto:best";
-// const cloudUrl = "https://res.cloudinary.com/dacsww4tg/image/upload/c_scale,w_300/q_95";
 const cloudUrl = "https://res.cloudinary.com/dacsww4tg/image/upload/c_scale,w_300";
 
 // Fetches image paths from a JSON file
@@ -53,7 +50,6 @@ const createImageLink = imageUrl => {
 
     image.classList.add('fade-in');
 
-    // const cleanedImageUrl = imageUrl.replace("/c_scale,w_300/q_auto:best", "");
     const cleanedImageUrl = imageUrl.replace("/c_scale,w_300", "");
     image.src = imageUrl;
     link.href = cleanedImageUrl;
@@ -69,9 +65,8 @@ const createPhotoGrid = async () => {
         console.log(`Number of links: ${paths.length}`);
         const imageUrls = paths.map(path => path.trim() !== "" ? `${cloudUrl}${path}` : "/pictures/1.jpg");
         const getRandomUrl = createUniqueRandomGenerator(imageUrls, picBuffer);
-        const getRandomIndex = createUniqueRandomGenerator([...Array(gridSize).keys()], indexBuffer);
 
-        for (let i = 1; i <= gridSize; i++) {
+        for (let i = 1; i <= paths.length; i++) {
             const cell = document.createElement("div");
             cell.className = "cell";
             const imageLink = createImageLink(getRandomUrl());
@@ -79,21 +74,7 @@ const createPhotoGrid = async () => {
             photoGrid.appendChild(cell);
         }
 
-        setInterval(() => {
-            const cells = photoGrid.querySelectorAll(".cell");
-            const cell = cells[getRandomIndex()];
-            const image = cell.querySelector("img");
-
-            image.classList.remove('fade-in');
-            image.src = getRandomUrl();
-            // console.log(`Image src: ${image.src.replace("/c_scale,w_300/q_auto:best", "")}`);
-            console.log(`Image src: ${image.src.replace("/c_scale,w_300", "")}`);
-
-            // Add a slight delay before re-adding the fade-in class to restart the animation
-            setTimeout(() => {
-                image.classList.add('fade-in');
-            }, 10);
-        }, refreshRate);
+        document.getElementById("info").innerHTML = `Total number of pictures: ${paths.length}`;
 
     } catch (error) {
         console.log('Fetch failed:', error);
